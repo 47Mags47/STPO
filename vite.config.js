7e2dd22de4911e953c87e052060f34f0fc787cef
47Mags47 <gord_ka47@mail.ru>
2024-06-10 11:07:07 +0700

    Загрузка модулей

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,25 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv  } from 'vite';
 import laravel from 'laravel-vite-plugin';
+
+const env = loadEnv('all', process.cwd());
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/sass/app.sass',
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
     ],
+    server: {
+        host: true,
+        port: env.VITE_ASSET_PORT,
+        strictPort: true,
+        hmr: {
+            host: env.VITE_ASSET_HOST,
+            port: env.VITE_ASSET_PORT,
+        },
+    },
 });
