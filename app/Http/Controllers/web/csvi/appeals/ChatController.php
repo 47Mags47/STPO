32405dd2edd036dc3b5b6cbfa305ca73ab0f0643
47Mags47 <gord_ka47@mail.ru>
2024-08-22 11:21:06 +0700

    back

--- app/Http/Controllers/web/csvi/appeals/ChatController.php
+++ app/Http/Controllers/web/csvi/appeals/ChatController.php
@@ -77,7 +77,6 @@ class ChatController
 
     public function download(Request $request){
         $path = $request->appeal . '/' . $request->file_name;
-        // return Storage::disk('appeal-chat')->download($path, mb_substr($request->file_name, 20));
-        dd(Storage::disk('appeal-chat')->has($path));
+        return Storage::disk('appeal-chat')->download($path, mb_substr($request->file_name, 20));
     }
 }
