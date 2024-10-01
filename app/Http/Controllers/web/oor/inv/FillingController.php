7130cca9b6aa0ea8601c0dc9f64cc3c7cbfdd412
47Mags47 <gord_ka47@mail.ru>
2024-10-01 08:46:21 +0700

    pre

--- app/Http/Controllers/web/oor/inv/FillingController.php
+++ app/Http/Controllers/web/oor/inv/FillingController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\web\oor\inv;
 
+use App\Models\Glossary\Glossary_Oor_inv_sheet;
 use App\Models\Oor\Oor_Inv_Data;
 use App\Models\Oor\Oor_Inv_InDate;
 use App\Models\Oor\Oor_Inv_Raport;
@@ -9,34 +10,52 @@ use Illuminate\Http\Request;
 
 class FillingController
 {
-    public function index(Request $request)
-    {
-        if (!$request->sheet) {
-            return redirect()->route('inv.filling.sheet', ['sheet' => 2]);
-        }
-
-        $sheet_id = $request->sheet ? $request->sheet : 2;
-        $page_data = PageController::fillingPageData($sheet_id);
-        return view('page.oor.inv.filling.index', $page_data);
-    }
-
-    public function fillingSave(Request $request)
-    {
+    public function hub(Request $request){
+        $in_date = Oor_Inv_InDate::actual();
+        $division = auth()->user()->division;
         $raport = Oor_Inv_Raport::firstOrCreate([
-            'division_id' => auth()->user()->division_id,
-            'in_date_id' => Oor_Inv_InDate::actual()->id,
-        ], [
-            'worker_id' => auth()->user()->id,
+            'in_date_id' => $in_date->id,
+            'division_id' => $division->id,
+        ],[
+            'worker_id' => auth()->user()->id
         ]);
-        foreach ($request->data as $coord => $value) {
-            Oor_Inv_Data::updateOrCreate([
-                'raport_id' => $raport->id,
-                'sheet_id' => $request->sheet,
-                'coord' => $coord
-            ], [
-                'value' => $value
-            ]);
-        }
-        return back()->with(['message' => 'Данные успешно сохранены']);
+
+        return redirect()->route('inv.filling.edit', ['raport' => $raport, 'sheet' => 2]);
     }
+
+    public function edit(Request $request, Oor_Inv_Raport $raport, Glossary_Oor_inv_sheet $sheet){
+        return view("page.oor.inv.filling.sheets.$sheet->id", compact('raport', 'sheet'));
+    }
+
+
+    // public function index(Request $request)
+    // {
+    //     if (!$request->sheet) {
+    //         return redirect()->route('inv.filling.sheet', ['sheet' => 2]);
+    //     }
+
+    //     $sheet_id = $request->sheet ? $request->sheet : 2;
+    //     $page_data = PageController::fillingPageData($sheet_id);
+    //     return view('page.oor.inv.filling.index', $page_data);
+    // }
+
+    // public function fillingSave(Request $request)
+    // {
+    //     $raport = Oor_Inv_Raport::firstOrCreate([
+    //         'division_id' => auth()->user()->division_id,
+    //         'in_date_id' => Oor_Inv_InDate::actual()->id,
+    //     ], [
+    //         'worker_id' => auth()->user()->id,
+    //     ]);
+    //     foreach ($request->data as $coord => $value) {
+    //         Oor_Inv_Data::updateOrCreate([
+    //             'raport_id' => $raport->id,
+    //             'sheet_id' => $request->sheet,
+    //             'coord' => $coord
+    //         ], [
+    //             'value' => $value
+    //         ]);
+    //     }
+    //     return back()->with(['message' => 'Данные успешно сохранены']);
+    // }
 }
--- resources/views/page/oor/inv/filling/sheets/2.blade.php
+++ resources/views/page/oor/inv/filling/sheets/2.blade.php
@@ -1,46 +1,99 @@
-<caption>{{ $sheet->name }}</caption>
-<thead>
-    <tr>
-        <x-table.th header value="Наименование <br> показателей" rowspan=3  />
-        <x-table.th value="№ строки" rowspan=3 />
-        <x-table.th value="Всего, чел." rowspan=3 />
-        <x-table.th value="в том числе" colspan=10 />
-    </tr>
-    <tr>
-        <x-table.th value="Женщины" colspan=2 />
-        <x-table.th value="Мужчины" colspan=2 />
-        <x-table.th value="в возрасте от 0 до 3 лет" colspan=2 />
-        <x-table.th value="в возрасте от 4 до 7 лет" colspan=2 />
-        <x-table.th value="в возрасте от 8 до 17 лет" colspan=2 />
-    </tr>
-    <tr>
-        <x-table.th value="всего" />
-        <x-table.th value="от 18 до 59 лет" />
-        <x-table.th value="всего" />
-        <x-table.th value="от 18 до 64 лет" />
-        <x-table.th value="мальчики" />
-        <x-table.th value="девочки" />
-        <x-table.th value="мальчики" />
-        <x-table.th value="девочки" />
-        <x-table.th value="мальчики" />
-        <x-table.th value="девочки" />
-    </tr>
-    <tr>
-        <x-table.th header value="1" />
-        <x-table.th value="2" />
-        <x-table.th value="3" />
-        <x-table.th value="4" />
-        <x-table.th value="5" />
-        <x-table.th value="6" />
-        <x-table.th value="7" />
-        <x-table.th value="8" />
-        <x-table.th value="9" />
-        <x-table.th value="10" />
-        <x-table.th value="11" />
-        <x-table.th value="12" />
-        <x-table.th value="13" />
-    </tr>
-</thead>
+@extends('page.oor.inv.filling.index')
+@section('sheet')
+    <x-table.box w100>
+        <x-slot:head>
+            <x-table.row>
+                <x-table.hcell val="Наименование показателей" rowspan=3 />
+                <x-table.hcell val="№ строки" rowspan=3 />
+                <x-table.hcell val="Всего, чел." rowspan=3 />
+                <x-table.hcell val="в том числе" colspan=10 />
+            </x-table.row>
+            <x-table.row>
+                <x-table.hcell val="Женщины" colspan=2 />
+                <x-table.hcell val="Мужчины" colspan=2 />
+                <x-table.hcell val="в возрасте от 0 до 3 лет" colspan=2 />
+                <x-table.hcell val="в возрасте от 4 до 7 лет" colspan=2 />
+                <x-table.hcell val="в возрасте от 8 до 17 лет" colspan=2 />
+            </x-table.row>
+            <x-table.row>
+                <x-table.hcell val="всего" />
+                <x-table.hcell val="от 18 до 59 лет" />
+                <x-table.hcell val="всего" />
+                <x-table.hcell val="от 18 до 64 лет" />
+                <x-table.hcell val="мальчики" />
+                <x-table.hcell val="девочки" />
+                <x-table.hcell val="мальчики" />
+                <x-table.hcell val="девочки" />
+                <x-table.hcell val="мальчики" />
+                <x-table.hcell val="девочки" />
+            </x-table.row>
+            <x-table.row>
+                <x-table.hcell val="1" />
+                <x-table.hcell val="2" />
+                <x-table.hcell val="3" />
+                <x-table.hcell val="4" />
+                <x-table.hcell val="5" />
+                <x-table.hcell val="6" />
+                <x-table.hcell val="7" />
+                <x-table.hcell val="8" />
+                <x-table.hcell val="9" />
+                <x-table.hcell val="10" />
+                <x-table.hcell val="11" />
+                <x-table.hcell val="12" />
+                <x-table.hcell val="13" />
+            </x-table.row>
+        </x-slot:head>
+        <x-slot:body>
+            <x-table.row input>
+                <x-table.cell val="Общая численность населения субъекта Российской Федерации, в том числе:" pd/>
+                <x-table.cell val="01" center pd/>
+                <x-table.cell><x-form.input formul="CA8+CA9" disabled /></x-table.cell>
+                <x-table.cell><x-form.input name="CB7" /></x-table.cell>
+                <x-table.cell><x-form.input name="CC7" /></x-table.cell>
+                <x-table.cell><x-form.input name="CD7" /></x-table.cell>
+                <x-table.cell><x-form.input name="CE7" /></x-table.cell>
+                <x-table.cell><x-form.input disabled /></x-table.cell>
+                <x-table.cell><x-form.input disabled /></x-table.cell>
+                <x-table.cell><x-form.input disabled /></x-table.cell>
+                <x-table.cell><x-form.input disabled /></x-table.cell>
+                <x-table.cell><x-form.input disabled /></x-table.cell>
+                <x-table.cell><x-form.input disabled /></x-table.cell>
+            </x-table.row>
+            <x-table.row input>
+                <x-table.cell val="Численность взрослого населения в возрасте 18 лет и старше" pd/>
+                <x-table.cell val="02" center pd />
+                <x-table.cell><x-form.input disabled /></x-table.cell>
+                <x-table.cell><x-form.input name="CB8" /></x-table.cell>
+                <x-table.cell><x-form.input name="CC8" /></x-table.cell>
+                <x-table.cell><x-form.input name="CD8" /></x-table.cell>
+                <x-table.cell><x-form.input name="CE8" /></x-table.cell>
+                <x-table.cell><x-form.input disabled /></x-table.cell>
+                <x-table.cell><x-form.input disabled /></x-table.cell>
+                <x-table.cell><x-form.input disabled /></x-table.cell>
+                <x-table.cell><x-form.input disabled /></x-table.cell>
+                <x-table.cell><x-form.input disabled /></x-table.cell>
+                <x-table.cell><x-form.input disabled /></x-table.cell>
+            </x-table.row>
+            <x-table.row input>
+                <x-table.cell val="Численность детского населения" pd/>
+                <x-table.cell val="03" center pd />
+                <x-table.cell><x-form.input disabled /></x-table.cell>
+                <x-table.cell><x-form.input disabled /></x-table.cell>
+                <x-table.cell><x-form.input disabled /></x-table.cell>
+                <x-table.cell><x-form.input disabled /></x-table.cell>
+                <x-table.cell><x-form.input disabled /></x-table.cell>
+                <x-table.cell><x-form.input name="CF9" /></x-table.cell>
+                <x-table.cell><x-form.input name="CG9" /></x-table.cell>
+                <x-table.cell><x-form.input name="CH9" /></x-table.cell>
+                <x-table.cell><x-form.input name="CI9" /></x-table.cell>
+                <x-table.cell><x-form.input name="CJ9" /></x-table.cell>
+                <x-table.cell><x-form.input name="CK9" /></x-table.cell>
+            </x-table.row>
+        </x-slot:body>
+    </x-table.box>
+@endsection
+
+{{-- <caption>{{ $sheet->name }}</caption>
 <tbody>
     <tr>
         <x-table.td type="header" value="Общая численность населения субъекта Российской Федерации, в том числе:" />
@@ -88,4 +141,4 @@
         <x-table.td type="input" inp-type="number" value="{{ $data->has('CK9') ? $data['CK9'] : '' }}" name="CK9" />
 
     </tr>
-</tbody>
+</tbody> --}}
