de7c0130c0fc713e0a75688e3849ee6beee200ac
47Mags47 <gord_ka47@mail.ru>
2025-03-11 10:46:27 +0700

    update and fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+
+const env = loadEnv('all', process.cwd());
 
 export default defineConfig({
     plugins: [
@@ -13,4 +15,13 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+    server: {
+        host: true,
+        port: env.VITE_ASSET_PORT,
+        strictPort: true,
+        hmr: {
+            host: env.VITE_ASSET_HOST,
+            port: env.VITE_ASSET_PORT,
+        },
+    },
 });
